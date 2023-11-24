6dd3ccc8ac417212f2e519d3014f51fc7a351b9b
Dario Becchio <dabek1986@gmail.com>
2023-11-24 19:21:32 +0100

    create form

--- scripr.js
+++ scripr.js
@@ -7,16 +7,22 @@ va applicato uno sconto del 40% per gli over 65.
 */
 
 const full_name = document.getElementById ( 'fullName' ).value
-document.getElementById('fullName').addEventListener('click' , function (){
+document.getElementById('save_name').addEventListener('click' , function (){
     console.log(document.getElementById('fullName').value);
 })
 
 
 const total_distance = document.getElementById ( 'distance' ).value
 console.log(total_distance);
+document.getElementById('save_distance').addEventListener('click' , function (){
+    console.log(document.getElementById('distance').value);
+})
 
 const your_age = document.getElementById ( 'age' ).value
 console.log(your_age);
+document.getElementById('save_age').addEventListener('click' , function (){
+    console.log(document.getElementById('age').value);
+})
 
 let gross_total =  (total_distance * 0.21).toFixed(2);
 console.log(gross_total);
@@ -24,13 +30,13 @@ console.log(gross_total);
 document.getElementById('result').addEventListener('click' , function (){
     if (your_age <= 18){
         const total_price = ( gross_total - (gross_total * 0.20) ).toFixed(2);
-        document.getElementById('result').innerHTML = (`Il tuo totale è ${total_price} €`) ;
+        document.getElementById('result').innerHTML = (`Il tuo totale è : ${total_price} €`) ;
     } else if (your_age >= 65){
         const total_price = ( gross_total - (gross_total * 0.40) ).toFixed(2);
-        document.getElementById('result').innerHTML = (`Il tuo totale è ${total_price} €`) ;
+        document.getElementById('result').innerHTML = (`Il tuo totale è : ${total_price} €`) ;
     } else{
     
-        document.getElementById('result').innerHTML = (`Il tuo totale è ${gross_total} €`) ;
+        document.getElementById('result').innerHTML = (`Il tuo totale è : ${gross_total} €`) ;
     }
 })
 
