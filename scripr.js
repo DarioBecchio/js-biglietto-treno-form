85561f3e2ede5b9711f3fcfae84248f083ee94fd
Dario Becchio <dabek1986@gmail.com>
2023-11-27 16:52:48 +0100

    Change form

--- scripr.js
+++ scripr.js
@@ -5,39 +5,62 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 */
+//Creo le costanti che mi serviranno
+const name_surname = document.getElementById('nomeCognome');
+const travel_lenght = document.getElementById('tot_km');
+const my_age = document.getElementById('Età');
+const save_input = document.getElementById('saveInput');
+const km_price = 0.21
+//Creo un evento per calcolare il totale
+save_input.addEventListener('click' , function (){
 
-const full_name = document.getElementById ( 'fullName' ).value
-document.getElementById('save_name').addEventListener('click' , function (){
-    console.log(document.getElementById('fullName').value);
-})
+  console.log('clicked'); 
+  const saved_age = parseInt(my_age.value)
+  const saved_km = parseInt(travel_lenght.value)
+  console.log(saved_age , saved_km);
+  const gross_total = ( saved_km * km_price ) ;
+  console.log(gross_total); 
+
+if (saved_age < 18){
+  const total_price = ( gross_total - (gross_total * 0.20) ).toFixed(2);
+  console.log(total_price);
+  document.getElementById('totale').innerHTML = (`Il tuo totale è ${total_price} €`) ;
+  
+} else if (saved_age > 65){
+  const total_price = ( gross_total - (gross_total * 0.40) ).toFixed(2);
+  document.getElementById('totale').innerHTML = (`Il tuo totale è ${total_price} €`) ;
+} else{
+
+  document.getElementById('totale').innerHTML = (`Il tuo totale è ${gross_total} €`) ;
+}
+
+}
+)
+
+/*
+//Creo una costante con il valore del nome e cognome
+const full_name = document.getElementById('fullName').value
+console.log(full_name);
 
 
-const total_distance = document.getElementById ( 'distance' ).value
+//Creo una costante con il valore dei km da percorrere
+
+const total_distance = document.getElementById('distance').value
 console.log(total_distance);
-document.getElementById('save_distance').addEventListener('click' , function (){
-    console.log(document.getElementById('distance').value);
-})
 
-const your_age = document.getElementById ( 'age' ).value
-console.log(your_age);
-document.getElementById('save_age').addEventListener('click' , function (){
-    console.log(document.getElementById('age').value);
-})
+document.getElementById('save').addEventListener('click' , function () {
+  console.log(full_name);  
+}
 
-let gross_total =  (total_distance * 0.21).toFixed(2);
-console.log(gross_total);
-
-document.getElementById('result').addEventListener('click' , function (){
-    if (your_age <= 18){
-        const total_price = ( gross_total - (gross_total * 0.20) ).toFixed(2);
-        document.getElementById('result').innerHTML = (`Il tuo totale è : ${total_price} €`) ;
-    } else if (your_age >= 65){
-        const total_price = ( gross_total - (gross_total * 0.40) ).toFixed(2);
-        document.getElementById('result').innerHTML = (`Il tuo totale è : ${total_price} €`) ;
-    } else{
-    
-        document.getElementById('result').innerHTML = (`Il tuo totale è : ${gross_total} €`) ;
-    }
-})
+)
 
 
+const my_age = document.getElementById('age').value
+console.log(my_age);
+
+document.getElementById('save').addEventListener('click' , function () {
+  console.log(full_name);  
+}
+
+)
+*/
